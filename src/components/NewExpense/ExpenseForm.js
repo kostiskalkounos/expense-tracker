384c456da297d513e18a417f4d95fca86dc3c885
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-08 21:01:28 +0200

    pass data from child to parent component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   /*
    * One state example
 
@@ -55,7 +55,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
